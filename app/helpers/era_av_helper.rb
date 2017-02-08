d7ed943cf1d923336b9f26912b4c507c708fb914
Chris Want <cjwant@gmail.com>
2017-02-08 15:29:33 -0700

    Issue #36. Make logo clickable and have it point to the home page. Also refactored this and some other U of A specific view code into an era_av helper.

---
+++ app/helpers/era_av_helper.rb
@@ -0,0 +1,29 @@
+# Copyright 2017, University of Alberta Libraries.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
+module EraAvHelper
+  def ualib_logo_link
+    link_to("", "https://library.ualberta.ca", :class => 'logo')
+  end
+
+  def era_av_logo_link
+    image =
+      image_tag("ERA-A+V-beta-logo.png",
+                alt: "University of Alberta ERA Audio and Video"\
+                "- #{t(:release_label)} #{Avalon::VERSION}")
+    return image if current_page?(root_path)
+    return link_to(image, root_path)
+  end
+
+end
