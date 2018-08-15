7aeb91dc71296d6a2e4e64a395e3278e34383303
Chris Want <cjwant@gmail.com>
2018-08-15 15:36:16 -0600

    Setup `resque` logging.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,6 +3,9 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 Resque.redis = Rails.application.secrets.redis_url
 
+Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
+Resque.logger.level = Logger::INFO
+
 # Enable schedule tab in Resque web ui
 require 'resque-scheduler'
 require 'resque/scheduler/server'
