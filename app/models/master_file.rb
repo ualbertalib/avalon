2130e14bd485d49933dba11d91a021a75ceb2422
Chris Want <cjwant@gmail.com>
2018-08-20 15:19:07 -0600

    Fix path that matterhorn encoder sees (#379)
    Since upgrade to Avalon 6.4.3, matterhorn isn't looking for files in the right place.

--- app/models/master_file.rb
+++ app/models/master_file.rb
@@ -276,8 +276,11 @@ class MasterFile < ActiveFedora::Base
   def matterhorn_path
     # This returns the URI path on the matterhorn server of the file who's path is on
     # the app server (so the path doesn't need to be the same on both servers)
-    new_path = file_location.gsub(Rails.application.secrets.matterhorn_client_media_path,
-                                  Rails.application.secrets.matterhorn_server_media_path)
+
+    # Prefer working_file_path, when available
+    old_path = working_file_path || file_location
+    new_path = old_path.gsub(Rails.application.secrets.matterhorn_client_media_path,
+                             Rails.application.secrets.matterhorn_server_media_path)
 
     FileLocator.new(new_path).uri.to_s
   end
