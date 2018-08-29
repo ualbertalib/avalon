e5004c42a40bc6f98780aa27a0c4b98032f1a2f5
Jeffery Antoniuk <jefferya@ualberta.ca>
2018-08-29 11:45:50 -0600

    add deposit agreement checkbox
    * add deposit agreement checkbox issue #374

--- app/assets/javascripts/license_form.js
+++ app/assets/javascripts/license_form.js
@@ -15,4 +15,32 @@ $(document).ready(function() {
       $('#media_object_terms_of_use_custom').addClass('hidden');
     }
   });
+
+  // deposit agreement checkbox
+  var checkbox = $("#agreement_checkbox");
+  var saveButton = $("input[value='Save']");
+  var saveAndContinueButton = $("input[value='Save and continue']");
+  checkbox.attr("checked",false);
+  if (checkbox.length>0) {
+    // if the #agreement_checkbox exists on the page then prevent save
+    // not present on the file upload page
+    saveButton.attr("disabled",true);
+    saveAndContinueButton.attr("disabled",true);
+  }
+  console.log(saveButton);
+
+  var cb = checkbox.change(function() {
+    var t = $(this).is(":checked");
+    if ($(this).is(":checked")) {
+      console.log("checked!");
+      saveButton.attr("disabled",false);
+      saveAndContinueButton.attr("disabled",false);
+    }
+    else {
+      console.log("unchecked!");
+      saveButton.attr("disabled",true);
+      saveAndContinueButton.attr("disabled",true);
+    }
+  });
+
 });
