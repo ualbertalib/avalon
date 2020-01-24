d426f4768475333a694af19cd1cdd50f9b40131b
Jeffery Antoniuk <jefferya@ualberta.ca>
2020-01-24 11:46:41 -0700

    Convert autogenerated ids from Rails 4 to Rails 5.1 bigint
    The two contexts:
    
    * Preexisting DB tables (Rails v4.x): autogenerated IDs of type int(11)
    * Newly created database (Rails v5.2): autogenerated IDs of type bigint(20)
    
    In Avalon 6.5, a DB migration adds a new table with foreign key (type specified) referencing the users table thus the db migration  successfully creates the table in the first context (preexisting DB) and errors on the second case (DB creation) due to a type mismatch.
    https://github.com/jefferya/avalon/blob/f55d4a16b4ce6efb4ee3568b7ac54134ab62851d/db/migrate/20190212211753_create_timelines.rb#L5
    
    Create a migration for the existing 5.4.3 deployment that changes all of the ID columns (and all of the columns that reference those ID columns
    
    The rationale for going this route is that the orm generator API may not last forever, so dealing with the change now is probably more future proof
    
    update database migration to include playlist clip_id
    
    include schema.rb after converting autogenerated ids
    * account for convert from Rails 4 default to Rails 5.1 biging ids

--- db/migrate/20190212211753_create_timelines.rb
+++ db/migrate/20190212211753_create_timelines.rb
@@ -1,5 +1,35 @@
 class CreateTimelines < ActiveRecord::Migration[5.1]
   def change
+    
+    # Rails v4.x to Rails v5.2: update default id columns from :integer to :bigint, auto_increment: true
+    change_column :annotations, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :annotations, :playlist_item_id, :bigint
+    change_column :api_tokens, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :batch_entries, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :batch_entries, :batch_registries_id, :bigint
+    change_column :batch_registries, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :batch_registries, :user_id, :bigint
+    change_column :bookmarks, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :bookmarks, :user_id, :bigint, null: false
+    change_column :courses, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :identities, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :ingest_batches, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :migration_statuses, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :minter_states, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :playlist_items, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :playlist_items, :playlist_id, :bigint, null: false
+    change_column :playlist_items, :clip_id, :bigint, null: false
+    change_column :playlists, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :playlists, :user_id, :bigint, null: false
+    change_column :role_maps, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :role_maps, :parent_id, :bigint
+    change_column :searches, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :searches, :user_id, :bigint
+    change_column :sessions, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :stream_tokens, :id, :bigint, null: false, unique: true, auto_increment: true
+    change_column :users, :id, :bigint, null: false, unique: true, auto_increment: true
+
+    # add timelines table: new feature
     create_table :timelines do |t|
       t.string :title
       t.references :user, foreign_key: true
